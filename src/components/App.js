ee5aed4fe347939102d4fae0916bb5ce68369ffc
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-07-12 15:18:43 -0600

    npm start is now working!

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
-import './App.css';
+import '../App.css';
 import Header from './Header';
+import Home from './Home'
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  
   return (
     <div className="App">
       <Header />
