4d04d485e98dc780bf225c2e7305902fe1a95848
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-07-12 15:45:04 -0600

    useEffect fetch to OpenBreweryDB and setting data to state. (URL for the fetch only grabs 20 random breweries from the state of Colorado at the moment

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,15 @@ import Home from './Home'
 import { useEffect, useState } from 'react';
 
 function App() {
+  const [breweries, setBreweries] = useState([])
+
+  console.log(breweries)
   
+  useEffect(() => {
+    fetch('https://api.openbrewerydb.org/breweries?by_state=colorado')
+    .then(resp => resp.json())
+    .then(data => setBreweries(data))
+  }, [])
   
   return (
     <div className="App">
