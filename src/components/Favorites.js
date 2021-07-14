142961e7389a85c44e705d524ecb7bbf5380e514
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-07-14 11:40:32 -0600

    added heading to Home and favorites, changed button functionality for favorites sorting

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,46 +1,70 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import FavoriteCard from './FavoriteCard'
 
 
 const Favorites = ({ favorites, setFavorites }) => {  
-    const [sortBy, setSortBy] = useState(false)
-    
-    const handleSort = (e) => {
-        setSortBy(e.target.checked)
-        const sortedFavoritesByState = [...favorites].sort(function(a,b) {
-            let nameA = a.state.toUpperCase()
-            let nameB = b.state.toUpperCase()
-            if (nameA < nameB) {
-                return -1
-            }
-            if(nameA > nameB) {
-                return 1
-            }
-        })
-        const sortedFavoritesByAlphabet = [...favorites].sort(function(a,b) {
-            let nameA = a.name.toUpperCase()
-            let nameB = b.name.toUpperCase()
-            if (nameA < nameB) {
-                return -1
-            }
-            if(nameA > nameB) {
-                return 1
-            }
-        })
-        e.target.checked ? setFavorites(sortedFavoritesByState) : setFavorites(sortedFavoritesByAlphabet)
+    const [sortByState, setSortByState] = useState(false)
+    const [sortByName, setSortByName] = useState(false)
+
+    const sortedFavoritesByAlphabet = [...favorites].sort(function(a,b) {
+        let nameA = a.name.toUpperCase()
+        let nameB = b.name.toUpperCase()
+        if (nameA < nameB) {
+            return -1
+        }
+        if(nameA > nameB) {
+            return 1
+        }
+    })
+
+    const sortedFavoritesByState = [...favorites].sort(function(a,b) {
+        let nameA = a.state.toUpperCase()
+        let nameB = b.state.toUpperCase()
+        if (nameA < nameB) {
+            return -1
+        }
+        if(nameA > nameB) {
+            return 1
+        }
+    })
+
+    const handleSortByState = (e) => {
+        setSortByState(e.target.checked)
+        if (e.target.checked) {
+            setFavorites(sortedFavoritesByState)
+            setSortByName(false)
+        } else {
+            setFavorites(sortedFavoritesByAlphabet)
+            setSortByName(true)
+        } 
+    }
+
+    const handleSortByName = (e) => {
+        setSortByName(e.target.checked)
+        if (e.target.checked) {
+            setFavorites(sortedFavoritesByAlphabet)
+            setSortByState(false)
+        } else {
+            setFavorites(sortedFavoritesByState)
+            setSortByState(true)
+        } 
     }
 
     return (
         <div>
-            <h1 className='favorites-header'>Favorites</h1>
+            <h1 className='favorites-header'>Favorite Breweries</h1>
             <div className='inline field'>
                 <div className='ui toggle checkbox'>
-                    <input onChange={handleSort} type='checkbox' tabindex='0' name='sort'></input>
-                    <label>Sort By Name or State</label>
+                    <input onChange={handleSortByState} type='checkbox' checked={sortByState} tabindex='0' name='sort'></input>
+                    <label>Sort By State</label>
+                </div><br></br><br></br>
+                <div className='ui toggle checkbox'>
+                    <input onChange={handleSortByName} type='checkbox' checked={sortByName} tabindex='0' name='sort'></input>
+                    <label>Sort By Name</label>
                 </div>     
             </div>
-            <h1>Sorted by {sortBy ? 'State' : 'Name'}</h1>
+            <br></br>
             <div className='ui link cards centered favorites'>
                 {favorites.map(favorite => {return (
                     <FavoriteCard 
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import GoogleMap from './GoogleMap'
 const Home = ({ breweries, favorites, handleChange, handleSubmit, handleFavorites, handleVisited, visitedBreweries }) => {
     return (
         <div className='home'>
+            <h1 className='favorites-header'>Search for Breweries by City!</h1>
             <Search 
                 handleChange={handleChange}
                 handleSubmit = {handleSubmit}
