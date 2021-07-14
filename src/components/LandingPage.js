7acd89872612ee07d1c58d4d8f6e73636187007b
John <jsjshellz@gmail.com>
2021-07-13 22:02:27 -0600

    added logo to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,7 +3,8 @@ import React from 'react'
 const LandingPage = () => {
     return (
         <div>
-            <h1 id='intro-text' className='animate__animated animate__backInDown'>Welcome to Deja-Bru!</h1>
+            <img className='animate__animated animate__backInDown' src='https://lh3.googleusercontent.com/3WuReiOZLoEp9Azzop0mIRhG8cIz5gDFMgJtZzH-oq4i2hD1fF6bsC8xoaLoaNE4e5KtM9KNf0--PNIgXbWnp4QqO-NO2jr1SeIFzu3qUiwqQog_HGzMVP8JwKSaO_74BPiOxFGZLe8_ccOy70-Rwg6GvUcDJy4hlymn1NN86xeba_jneV5G21JEYFqgT0V2r-6EwwdhWIOHNztG3Xq-FnSpx16Eewxjl7kKMpcOYIWcxHxAVd0liXcZSeS4JHcWx-rha7ZeevnNOWcSFDjiV2jmyYutUOPyCUcBItOHXNp_qn8LLis4O6_79pe31r9TgahrBn0Y34rBpxL6K87QkbjKjwYsq7we3TJEtRi_KP_ucn_KEP4sZwEhwunNHHgIhZ9h_bDGgMVg7wyvykwXeapHJOsn1wQTx4WZSF9MxRer8U9-OvEGd3gvnK4Kmw8n7fHNbrAna42YLdYxMoNb-AVZNnDsxkQhgc-wSK1umzorWPeHpw7uBpBR8f_jMzF94b0JzwOZtww_xXw49iHFfYvkWuKWVjxwxfaqTD6lvFaUmtzfTcTCAV98119NqJlzPz83U4YQai55pgi0kPtHgZuOquGZCav_pwdY35jMQyCaoMqpkZQVaDYdn0EBPhN9Jqs72svqjFRXjCtauRYjYGYLuVHFWE01d3A_ftHZpIaYaQM1ijK3AJWxJ7rV5ba7UB8A03mqiF4Y3lejk8ef7w=s500-no?authuser=0'></img>
+            {/* <h1 id='intro-text' className='animate__animated animate__backInDown'>Welcome to Deja-Bru!</h1> */}
             <div id="background-wrap" className='animate__animated animate__slideInUp'>
                 <div class="bubble x1"></div>
                 <div class="bubble x2"></div>
