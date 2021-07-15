0e160a2ed2824ebc44343802bb97b41a8f6b5630
John <jsjshellz@gmail.com>
2021-07-15 12:50:14 -0600

    styled comments - added new buttons

--- src/components/FavoriteCard.js
+++ src/components/FavoriteCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {FaTrash} from "react-icons/fa"
 import {FaCommentAlt} from "react-icons/fa"
 import {FaPlus} from "react-icons/fa"
+import {FaEdit} from "react-icons/fa"
 import { useState } from 'react'
 
 const FavoriteCard = ({ id, brewery_type, name, city, state, website_url, favorites, setFavorites, favoritesComment}) => {
@@ -9,6 +10,7 @@ const FavoriteCard = ({ id, brewery_type, name, city, state, website_url, favori
 const [showComments, setShowComments] = useState(false)
 const [comments, setComments] = useState('')
 const [submittedComment, setSubmittedComment ] = useState(favoritesComment)
+const [editComment, setEditComment] = useState(false)
 
 
     function handleFavoriteComments(){
@@ -18,6 +20,10 @@ const [submittedComment, setSubmittedComment ] = useState(favoritesComment)
     function handleInputChange(e){
         let value = e.target.value
         setComments(value)
+    }
+
+    function toggleEdit(){
+        setEditComment(!editComment)
     }
 
     
@@ -32,6 +38,7 @@ const [submittedComment, setSubmittedComment ] = useState(favoritesComment)
             body: JSON.stringify({ comment: newComment })
         })
         setSubmittedComment(newComment)
+        setEditComment(!editComment)
     }
 
 
@@ -52,15 +59,16 @@ const [submittedComment, setSubmittedComment ] = useState(favoritesComment)
                     {website_url ? <a href={website_url}>Visit Brewery Website</a> : <p>No Website Available 😒 </p>}
                 </div>
             </div>
-            <span className='comments' onClick={handleFavoriteComments}>&nbsp;&nbsp;{!showComments ?  <p><FaCommentAlt />&nbsp;&nbsp; View Comments <img class="ui avatar image" src='https://lh3.googleusercontent.com/pw/AM-JKLWOfnbaH-tiA83auwm7QzXHmhLNTdv86g0k6OsYmbdtoZAhvJnXngldCZIBCo33WMT5zjaVxFw0ygqiVBzaoBWqiqvOsrnkVPzgL6yAqPs7xVhFlkqh018T2wRb7CjHjUG6q0JlNnMn4DdwoMEKR2E=s858-no?authuser=0'></img></p> : 'Add Comment'}</span>
+            <span className='comments' onClick={handleFavoriteComments}>&nbsp;&nbsp;{!showComments ?  <p><FaCommentAlt />&nbsp; View Comments <img class="ui avatar image" src='https://lh3.googleusercontent.com/pw/AM-JKLWOfnbaH-tiA83auwm7QzXHmhLNTdv86g0k6OsYmbdtoZAhvJnXngldCZIBCo33WMT5zjaVxFw0ygqiVBzaoBWqiqvOsrnkVPzgL6yAqPs7xVhFlkqh018T2wRb7CjHjUG6q0JlNnMn4DdwoMEKR2E=s858-no?authuser=0'></img></p> : <p><FaCommentAlt />&nbsp;Exit Comments</p>}</span>
             <div class='extra content'>
             { !showComments ? null : <div class="ui message">
-                <ul class="list">
+                <p>{submittedComment}&nbsp;&nbsp;<FaEdit onClick={toggleEdit}/></p>
+            { !editComment ? null :                 <ul class="list">
                     <form onSubmit={handleSubmit}>
                         <input name='comment' onChange={handleInputChange} type='text'></input>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<FaPlus onClick={handleSubmit}/>
                     </form>
-                <p>{submittedComment}</p>
-                </ul>
+                </ul> }
+
             </div>}
             </div>
             <div onClick={handleDelete} className='ui button attached button' id='delete-btn'>{<FaTrash />}&nbsp;&nbsp;Remove From Favorites</div>
