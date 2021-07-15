cd7bdee368e633f28cb309acc117679d3b7aec31
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-07-15 16:57:14 -0600

    fixed map styling

--- src/components/VisitedBreweriesCollection.js
+++ src/components/VisitedBreweriesCollection.js
@@ -30,11 +30,11 @@ const VisitedBreweriesCollection = ({ visitedBreweries, setVisitedBreweries, fav
     }
     
     return (
-        <div>
+        <div id='visited-section'>
             <h1 className='favorites-header'>Visited Breweries</h1>
-                <GoogleMapVisited
-                    visitedBreweries={visitedBreweries}
-                />
+            <GoogleMapVisited
+                visitedBreweries={visitedBreweries}
+            />
             <div className="searchbar" id='visitedSearch'>
                 <label htmlFor="search">Search Your Visited Breweries:</label>&nbsp;&nbsp;
                 <input
@@ -44,46 +44,48 @@ const VisitedBreweriesCollection = ({ visitedBreweries, setVisitedBreweries, fav
                     onChange={handleSearchText}
                 />
             </div>   
-            <span id='filterByStars'>
-            <label htmlFor="search">Filter By Rating</label>&nbsp;&nbsp;
-            <select onChange={handleRating}>
-                <option value='' defaultValue='select'>Show All</option>
-                <option value="1">1 Star</option>
-                <option value="2">2 Stars</option>
-                <option value="3">3 Stars</option>
-                <option value="4">4 Stars</option>
-                <option value="5">5 Stars</option>
-            </select></span>
-            <table className='ui celled collapsing padded striped table'>
-                <thead>
-                    <tr><th>Add to Favorites</th>
-                    <th>Name</th>
-                    <th>Location</th>
-                    <th>Rating</th>
-                    <th>Website</th>
-                </tr></thead>
-                <tbody className='section'>
-                {handleFilter().map(brewery => {
-                if (brewery.name.toLowerCase().includes(searchText.toLowerCase()))
-                    return (
-                    <VisitedBreweriesCard 
-                        key={brewery.id}
-                        id={brewery.id}
-                        name={brewery.name}
-                        city={brewery.city}
-                        state={brewery.state}
-                        website_url={brewery.website_url}
-                        favorites={favorites}
-                        visitedBreweries={visitedBreweries}
-                        brewery={brewery}
-                        rating={brewery.rating}
-                        submittedRating={submittedRating}
-                        setSubmittedRating={setSubmittedRating}
-                        handleFavorites={handleFavorites}
-                    />
-                )})}
-                </tbody>
-            </table> 
+            <div>
+                <span>
+                <label htmlFor="search">Filter By Rating</label>&nbsp;&nbsp;
+                <select onChange={handleRating}>
+                    <option value='' defaultValue='select'>Show All</option>
+                    <option value="1">1 Star</option>
+                    <option value="2">2 Stars</option>
+                    <option value="3">3 Stars</option>
+                    <option value="4">4 Stars</option>
+                    <option value="5">5 Stars</option>
+                </select></span>
+                <table className='ui celled collapsing padded striped table'>
+                    <thead>
+                        <tr><th>Add to Favorites</th>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th>Rating</th>
+                        <th>Website</th>
+                    </tr></thead>
+                    <tbody className='section'>
+                    {handleFilter().map(brewery => {
+                    if (brewery.name.toLowerCase().includes(searchText.toLowerCase()))
+                        return (
+                        <VisitedBreweriesCard 
+                            key={brewery.id}
+                            id={brewery.id}
+                            name={brewery.name}
+                            city={brewery.city}
+                            state={brewery.state}
+                            website_url={brewery.website_url}
+                            favorites={favorites}
+                            visitedBreweries={visitedBreweries}
+                            brewery={brewery}
+                            rating={brewery.rating}
+                            submittedRating={submittedRating}
+                            setSubmittedRating={setSubmittedRating}
+                            handleFavorites={handleFavorites}
+                        />
+                    )})}
+                    </tbody>
+                </table>
+            </div> 
             <Footer />   
         </div>
     )
